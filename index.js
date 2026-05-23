c9d981ba2b4196792ae769664abd740f3fd9b751
dataapk <onlinedatabase.expert@gmail.com>
2026-05-23 15:41:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-<script src="index.js"></script>
+setInterval(() => {
+  const pairs = ["BTC/USDT", "ETH/USDT", "BNB/USDT"];
+
+  const signal = {
+    pair: pairs[Math.floor(Math.random() * pairs.length)],
+    type: Math.random() > 0.5 ? "BUY" : "SELL",
+    strength: Math.floor(Math.random() * 40) + 60
+  };
+
+  console.log(signal);
+}, 3000);
