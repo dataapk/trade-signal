a43e35c2e34d627ef4c4c8d3baa16c31133b11db
dataapk <onlinedatabase.expert@gmail.com>
2026-05-25 10:40:34 +0600

    Update superbase.js

--- superbase.js
+++ superbase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://nrwhupzgdwlsdnwdfpig.supabase.co";
 
+const SUPABASE_ANON_KEY = "sb_publishable_4I-i-becqQZHBXK-skXDfA_gQLOID3a";
+
+const client = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
